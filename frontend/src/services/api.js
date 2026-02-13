333d31d9cc8ab5e50add5cedfefe73b38685e8cd
Aditya Maurya <adityapbh028@gmail.com>
2026-02-13 15:23:39 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // services/api.js
 import axios from 'axios';
 
-const API_URL ='http://localhost:5000/api';
+const API_URL ='https://aa-i4j8.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
